6cb87a8ba828db6d77f8223a6e11dacb6293d3ff
deshpandeanoop <anoop.bhardwaja@gmail.com>
2021-03-28 12:08:40 +0530

    Initial commit

---
+++ src/main/java/com/explore/parquet/ExploreApacheParquetApplication.java
@@ -0,0 +1,13 @@
+package com.explore.parquet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreApacheParquetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreApacheParquetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/explore/parquet/ExploreApacheParquetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explore.parquet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreApacheParquetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
